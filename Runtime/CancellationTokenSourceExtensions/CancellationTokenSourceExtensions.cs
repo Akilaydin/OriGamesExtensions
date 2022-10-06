2fc3823613202b26cc3e370204ce7ebdb80b31d5
Artem Ovchinnikov <artem.o@amayasoft.uz>
2022-10-06 20:25:15 +0300

    Cts extensions added

---
+++ Runtime/CancellationTokenSourceExtensions/CancellationTokenSourceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace OriGames.Extensions.CancellationTokenSourceExtensions
+{
+	public static class CancellationTokenSourceExtensions
+	{
+		public static void Destroy(this CancellationTokenSource cts)
+		{
+			cts.Cancel();
+			cts.Dispose();
+		}
+	}
+}
