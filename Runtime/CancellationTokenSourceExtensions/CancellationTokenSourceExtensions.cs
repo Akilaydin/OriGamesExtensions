5a7f2b07effeaaeb788a71819c9baaef47503dff
Artem Ovchinnikov <artem.o@amayasoft.uz>
2022-10-06 20:27:13 +0300

    Summary added

--- Runtime/CancellationTokenSourceExtensions/CancellationTokenSourceExtensions.cs
+++ Runtime/CancellationTokenSourceExtensions/CancellationTokenSourceExtensions.cs
@@ -4,6 +4,10 @@ namespace OriGames.Extensions.CancellationTokenSourceExtensions
 {
 	public static class CancellationTokenSourceExtensions
 	{
+		/// <summary>
+		/// Cancels and Disposes the given CancellationTokenSource
+		/// </summary>
+		/// <param name="cts"></param>
 		public static void Destroy(this CancellationTokenSource cts)
 		{
 			cts.Cancel();
